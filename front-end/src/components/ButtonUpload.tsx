b8f7a80d4a685727c262cd15faefe199f10dd1da
Matheus Lacerda <math2.0@hotmail.com>
2023-11-12 18:35:32 -0300

    Dnd audio, video, images

---
+++ front-end/src/components/ButtonUpload.tsx
@@ -0,0 +1,104 @@
+import React, { useState, useRef, CSSProperties } from 'react';
+import { MediaItem, MediaPosition } from './DndBoard';
+
+const ButtonUpload: React.FC = () => {
+  const [isUploadOpen, setIsUploadOpen] = useState(false);
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
+  const handleUploadButtonClick = () => {
+    if (fileInputRef.current) {
+      fileInputRef.current.click();
+    }
+  };
+
+  const handleFileInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+
+    const maxSize = 1024 * 1024 * 24; //That's 24Mb
+    const file = event.target.files && event.target.files[0];
+
+    if(!file){
+        console.warn("algo de errado nao esta certo");
+        return;
+    }
+
+    if (file.size > maxSize) {
+        console.warn('File size exceeds 32MB:', file.name);
+        return;
+    }
+
+    let type: MediaItem['type'];
+    const defaultPosition: MediaPosition = {x: 0, y: 0 };
+
+    if (file.type.startsWith('image/')) {
+        type = 'image';
+    } else if (file.type.startsWith('audio/')) {
+        type = 'audio';
+        defaultPosition.x = 40;
+    } else if (file.type.startsWith('video/')) {
+        type = 'video';
+    } else if (file.type === 'text/plain') {
+        type = 'text';
+    } else {
+        console.warn('Unsupported file format:', file.name);
+        return;
+    }
+    
+    const newItem: MediaItem = {
+        type,
+        src: URL.createObjectURL(file),
+        name: file.name
+    };
+    /*
+    setMedia((prevMedia) => [...prevMedia, newItem]);
+    setPositions([...positions, defaultPosition]);
+    setInitialPositions([...initialPositions, defaultPosition]);
+    */
+    // Reset the file input to allow selecting the same file again
+    event.target.value = '';
+  };
+
+  const style: CSSProperties = {
+    position: 'fixed',
+    top: 0,
+    right: 0,
+    margin: '10px',
+    width: '40px',
+    height: '40px',
+    padding: 0,
+    border: 'none',
+    background: 'transparent'
+  };
+
+  return (
+    <div className="board-button">
+        
+      <button
+        style={style}
+        onClick={handleUploadButtonClick}
+      >
+        <img
+          src="../images/me.jpeg"
+          alt="Upload Button"
+          style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+        />
+      </button>
+      
+      <input
+        type="file"
+        ref={fileInputRef}
+        style={{ display: 'none' }}
+        onChange={handleFileInputChange}
+      />
+  
+      {isUploadOpen && (
+        <div>
+          <p>Upload Window</p>
+          <button onClick={() => setIsUploadOpen(false)}>Close</button>
+        </div>
+      )}
+
+    </div>
+  );  
+};
+
+export default ButtonUpload;
---
+++ front-end/src/components/DndBoard.tsx
@@ -0,0 +1,182 @@
+import React, { useEffect, useState, CSSProperties } from 'react';
+import DndImage from './DndImage';
+import DndVideo from './DndVideo';
+import DndAudio from './DndAudio';
+import ButtonUpload from './ButtonUpload';
+import '../App.css';
+
+interface MediaItem {
+  type: 'image' | 'video' | 'audio' | 'text';
+  src: string;
+  name: string;
+}
+
+interface MediaPosition {
+  x: number;
+  y: number;
+}
+
+const DndBoard: React.FC = () => {
+  
+  const [media, setMedia] = useState<MediaItem[]>([]);
+  const [positions, setPositions] = useState<MediaPosition[]>([]);
+  const [dragging, setDragging] = useState<number | null>(null);
+  const [initialPositions, setInitialPositions] = useState<MediaPosition[]>([]);
+  const [selected, setSelected] = useState<number | null>(null);
+
+  const handleDelete = (event: KeyboardEvent) => {
+    if (event.key === 'Delete' && selected !== null) {
+      setMedia((prevMedia) => prevMedia.filter((_, index) => index !== selected));
+      setPositions((prevPositions) => prevPositions.filter((_, index) => index !== selected));
+      setInitialPositions((prevInitialPositions) => prevInitialPositions.filter((_, index) => index !== selected));
+      setSelected(null);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleDelete);
+
+    return () => {
+      document.removeEventListener('keydown', handleDelete);
+    };
+  });
+
+  const handleMediaClick = (index: number) => {
+    setSelected(index);
+  };
+
+  const handleMouseDown = (e: React.MouseEvent<HTMLDivElement>, index: number) => {
+    setSelected(index);
+    setDragging(index);
+
+    const offSetX = e.nativeEvent.offsetX;
+    const offSetY = e.nativeEvent.offsetY;
+
+    setInitialPositions([
+      ...initialPositions,
+      {
+        x: offSetX,
+        y: offSetY,
+      },
+    ]);
+  };
+
+  const handleMouseUp = () => {
+    setDragging(null);
+  };
+
+  const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
+    if (dragging !== null) {
+      const updatedPositions = [...positions];
+      updatedPositions[dragging] = {
+        x: e.clientX - initialPositions[dragging].x,
+        y: e.clientY - initialPositions[dragging].y,
+      };
+      setPositions(updatedPositions);
+    }
+  };
+
+  const handleDrop = (eve: React.DragEvent<HTMLDivElement>) => {
+    eve.preventDefault();
+  
+    const files = eve.dataTransfer.files;
+  
+    for (let i = 0; i < files.length; i++) {
+
+      const file = files[i];
+      const maxSize = 1024 * 1024 * 24; //That's 24Mb
+
+      if (file.size > maxSize) {
+        console.warn('File size exceeds 32MB:', file.name);
+        continue;
+      }
+  
+      let type: MediaItem['type'];
+      const defaultPosition: MediaPosition = {x: 0, y: 0 };
+  
+      if (file.type.startsWith('image/')) {
+        type = 'image';
+      } else if (file.type.startsWith('audio/')) {
+        type = 'audio';
+        defaultPosition.x = 40;
+      } else if (file.type.startsWith('video/')) {
+        type = 'video';
+      } else if (file.type === 'text/plain') {
+        type = 'text';
+      } else {
+        console.warn('Unsupported file format:', file.name);
+        continue;
+      }
+  
+      const newItem: MediaItem = {
+        type,
+        src: URL.createObjectURL(file),
+        name: file.name
+      };
+  
+      setMedia((prevMedia) => [...prevMedia, newItem]);
+      setPositions([...positions, defaultPosition]);
+      setInitialPositions([...initialPositions, defaultPosition]);
+    }
+  };
+
+  const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div
+      className="drop-area"
+      onDrop={handleDrop}
+      onDragOver={handleDragOver}
+      onMouseUp={handleMouseUp}
+      onMouseMove={handleMouseMove}
+    >
+      {media.map((item, index) => {
+        const isSelected = selected === index;
+
+        const style: CSSProperties = {
+          position: 'absolute',
+          left: positions[index]?.x || 0,
+          top: positions[index]?.y || 0,
+          cursor: dragging === index ? 'grabbing' : 'grab',
+          border: isSelected ? '1px solid purple' : 'none',
+          padding: '12px'
+        };
+
+        return item.type === 'image' ? (
+          <DndImage
+            key={index}
+            src={item.src}
+            style={style}
+            isSelected={isSelected}
+            onMouseDown={(e) => handleMouseDown(e, index)}
+            onClick={() => handleMediaClick(index)}
+          />
+        ) : item.type === 'video' ? (
+          <DndVideo
+            key={index}
+            src={item.src}
+            style={style}
+            isSelected={isSelected}
+            onMouseDown={(e) => handleMouseDown(e, index)}
+            onClick={() => handleMediaClick(index)}
+          />
+        ) : (
+          <DndAudio
+            key={index}
+            src={item.src}
+            name={item.name}
+            style={style}
+            isSelected={isSelected}
+            onMouseDown={(e) => handleMouseDown(e, index)}
+            onClick={() => handleMediaClick(index)}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default DndBoard;
+export { MediaItem, MediaPosition };
