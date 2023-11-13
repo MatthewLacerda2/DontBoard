225672a72652fb66e46279622b60ade11d8738a6
Matheus Lacerda <math2.0@hotmail.com>
2023-11-13 02:41:10 -0300

    scaler component

--- front-end/src/components/DndImage.tsx
+++ front-end/src/components/DndImage.tsx
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Scaler from './Scaler';
 import { DndMediaProps } from './DndBoard';
 
-const DndImage: React.FC<DndMediaProps> = ({ src, name, style, isSelected, onMouseDown, onClick }) => (
-  <div
-    className={`draggable-media ${isSelected ? 'selected' : ''}`}
-    onMouseDown={onMouseDown}
-    onClick={onClick}
-    style={style}
-  >
-    <img src={src} alt="Dropped" className="draggable-media" />
-  </div>
-);
+const DndImage: React.FC<DndMediaProps> = ({ src, name, style, isSelected, onMouseDown, onClick }) => {
+  const [width, setWidth] = useState<number>(style.width as number);
+  const [height, setHeight] = useState<number>(style.height as number);
+
+  const handleResize = (newWidth: number, newHeight: number) => {
+    setWidth(newWidth);
+    setHeight(newHeight);
+  };
+
+  return (
+    <Scaler onResize={handleResize}>
+      <div
+        className={`draggable-media ${isSelected ? 'selected' : ''}`}
+        onMouseDown={onMouseDown}
+        onClick={onClick}
+        style={{ ...style, width, height }}
+      >
+        <img src={src} alt={name} className="draggable-media" />
+      </div>
+    </Scaler>
+  );
+};
 
 export default DndImage;
---
+++ front-end/src/components/Scaler.tsx
@@ -0,0 +1,66 @@
+import React, { useState, useRef, useEffect, MouseEvent } from 'react';
+
+interface ScalerProps {
+    onResize: (width: number, height: number) => void;
+    children?: React.ReactNode;
+}
+
+  const Scaler: React.FC<ScalerProps> = ({ onResize, children }) => {
+  const [resizing, setResizing] = useState<boolean>(false);
+  const [startX, setStartX] = useState<number>(0);
+  const [startY, setStartY] = useState<number>(0);
+  const [initialWidth, setInitialWidth] = useState<number>(0);
+  const [initialHeight, setInitialHeight] = useState<number>(0);
+
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleMouseMove = (e: MouseEvent) => {
+      if (resizing && containerRef.current) {
+        const deltaX = e.clientX - startX;
+        const deltaY = e.clientY - startY;
+  
+        const newWidth = initialWidth + deltaX;
+        const newHeight = initialHeight + deltaY;
+  
+        onResize(newWidth, newHeight);
+      }
+    };
+  
+    const handleMouseUp = () => {
+      setResizing(false);
+    };
+  
+    if (resizing) {
+      document.addEventListener('mousemove', handleMouseMove as unknown as EventListener);
+      document.addEventListener('mouseup', handleMouseUp);
+  
+      return () => {
+        document.removeEventListener('mousemove', handleMouseMove as unknown as EventListener);
+        document.removeEventListener('mouseup', handleMouseUp);
+      };
+    }
+  }, [resizing, startX, startY, initialWidth, initialHeight, onResize]);
+  
+  const handleMouseDown = (e: React.MouseEvent) => {
+    if (containerRef.current) {
+      setResizing(true);
+      setStartX(e.clientX);
+      setStartY(e.clientY);
+      setInitialWidth(containerRef.current.clientWidth);
+      setInitialHeight(containerRef.current.clientHeight);
+    }
+  };
+
+  return (
+    <div
+      className="scaler-container"
+      ref={containerRef}
+      onMouseDown={handleMouseDown}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Scaler;
