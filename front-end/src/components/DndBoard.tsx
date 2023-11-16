5e3a526c19f0870970cf652ddd8fa4163b062165
Matheus Lacerda <math2.0@hotmail.com>
2023-11-16 02:12:08 -0300

    merge with main after dndtext push

--- front-end/src/components/DndBoard.tsx
+++ front-end/src/components/DndBoard.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState, CSSProperties } from 'react';
 import DndImage from './DndImage';
 import DndVideo from './DndVideo';
 import DndAudio from './DndAudio';
+import DndText from './DndText';
 import '../App.css';
 
 interface DndMediaProps {
@@ -17,6 +18,7 @@ interface MediaItem {
   type: 'image' | 'video' | 'audio' | 'text';
   src: string;
   name: string;
+  text?: string;
 }
 
 interface MediaPosition {
@@ -32,22 +34,33 @@ const DndBoard: React.FC = () => {
   const [initialPositions, setInitialPositions] = useState<MediaPosition[]>([]);
   const [selected, setSelected] = useState<number | null>(null);
 
-  const handleDelete = (event: KeyboardEvent) => {
-    if (event.key === 'Delete' && selected !== null) {
-      setMedia((prevMedia) => prevMedia.filter((_, index) => index !== selected));
-      setPositions((prevPositions) => prevPositions.filter((_, index) => index !== selected));
-      setInitialPositions((prevInitialPositions) => prevInitialPositions.filter((_, index) => index !== selected));
-      setSelected(null);
-    }
-  };
-
   useEffect(() => {
+    const handleDelete = (event: KeyboardEvent) => {
+      if (event.key === 'Delete' && selected !== null) {
+        setMedia((prevMedia) => prevMedia.filter((_, index) => index !== selected));
+        setPositions((prevPositions) => prevPositions.filter((_, index) => index !== selected));
+        setInitialPositions((prevInitialPositions) => prevInitialPositions.filter((_, index) => index !== selected));
+        setSelected(null);
+      }
+    };
+  
+    const handleDocumentClick = (event: MouseEvent) => {
+      const target = event.target as HTMLElement;
+  
+      // Check if the clicked element is outside the selected item
+      if (selected !== null && !target.closest('.draggable-media')) {
+        setSelected(null);
+      }
+    };
+  
     document.addEventListener('keydown', handleDelete);
-
+    document.addEventListener('click', handleDocumentClick);
+  
     return () => {
       document.removeEventListener('keydown', handleDelete);
+      document.removeEventListener('click', handleDocumentClick);
     };
-  });
+  }, [selected]);  
 
   const handleMediaClick = (index: number) => {
     setSelected(index);
@@ -75,6 +88,8 @@ const DndBoard: React.FC = () => {
 
   const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
     if (dragging !== null) {
+      e.preventDefault();
+
       const updatedPositions = [...positions];
       updatedPositions[dragging] = {
         x: e.clientX - initialPositions[dragging].x,
@@ -82,25 +97,38 @@ const DndBoard: React.FC = () => {
       };
       setPositions(updatedPositions);
     }
+  };    
+
+  const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
   };
+
+  const handleTextChange = (index: number, newText: string) => {
+    const updatedMedia = [...media];
+    updatedMedia[index].text = newText;
+    setMedia(updatedMedia);
+  };  
 
   const handleDrop = (eve: React.DragEvent<HTMLDivElement>) => {
     eve.preventDefault();
+
+    if (dragging !== null) {
+      return;
+    }
   
     const files = eve.dataTransfer.files;
   
     for (let i = 0; i < files.length; i++) {
-
       const file = files[i];
-      const maxSize = 1024 * 1024 * 24; //That's 24Mb
-
+      const maxSize = 1024 * 1024 * 24; // 24MB
+  
       if (file.size > maxSize) {
-        console.warn('File size exceeds 32MB:', file.name);
+        console.warn('File size exceeds 24MB:', file.name);
         continue;
       }
   
       let type: MediaItem['type'];
-      const defaultPosition: MediaPosition = {x: 0, y: 0 };
+      const defaultPosition: MediaPosition = { x: 0, y: 0 };
   
       if (file.type.startsWith('image/')) {
         type = 'image';
@@ -111,25 +139,39 @@ const DndBoard: React.FC = () => {
         type = 'video';
       } else if (file.type === 'text/plain') {
         type = 'text';
+        // Read the text content of the text file
+        const reader = new FileReader();
+        reader.onload = (event) => {
+          const textContent = event.target?.result as string;
+          const newItem: MediaItem = {
+            type,
+            src: URL.createObjectURL(file),
+            name: file.name,
+            text: textContent,
+          };
+  
+          setMedia((prevMedia) => [...prevMedia, newItem]);
+          setPositions([...positions, defaultPosition]);
+          setInitialPositions([...initialPositions, defaultPosition]);
+        };
+        reader.readAsText(file);
       } else {
         console.warn('Unsupported file format:', file.name);
         continue;
       }
   
-      const newItem: MediaItem = {
-        type,
-        src: URL.createObjectURL(file),
-        name: file.name
-      };
+      if (type !== 'text') {
+        const newItem: MediaItem = {
+          type,
+          src: URL.createObjectURL(file),
+          name: file.name,
+        };
   
-      setMedia((prevMedia) => [...prevMedia, newItem]);
-      setPositions([...positions, defaultPosition]);
-      setInitialPositions([...initialPositions, defaultPosition]);
+        setMedia((prevMedia) => [...prevMedia, newItem]);
+        setPositions([...positions, defaultPosition]);
+        setInitialPositions([...initialPositions, defaultPosition]);
+      }
     }
-  };
-
-  const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
-    e.preventDefault();
   };
 
   return (
@@ -172,7 +214,7 @@ const DndBoard: React.FC = () => {
             onMouseDown={(e) => handleMouseDown(e, index)}
             onClick={() => handleMediaClick(index)}
           />
-        ) : (
+        ) : item.type === 'audio' ?  (
           <DndAudio
             key={index}
             src={item.src}
@@ -181,6 +223,15 @@ const DndBoard: React.FC = () => {
             isSelected={isSelected}
             onMouseDown={(e) => handleMouseDown(e, index)}
             onClick={() => handleMediaClick(index)}
+          />
+        ) : (
+          <DndText
+            key={index}
+            text={item.text || ''}
+            style={style}
+            isSelected={isSelected}
+            onMouseDown={(e) => handleMouseDown(e, index)}
+            onTextChange={(newText) => handleTextChange(index, newText)}
           />
         );
       })}
