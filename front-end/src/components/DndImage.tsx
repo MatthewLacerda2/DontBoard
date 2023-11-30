48fde801663c6cc65f58d734f231178e56e41fda
Matheus Lacerda <matheus.l1996@gmail.com>
2023-11-30 17:39:23 -0300

    zoom scaling and infinite page

--- front-end/src/components/DndImage.tsx
+++ front-end/src/components/DndImage.tsx
@@ -1,15 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Scaler from './Scaler';
 import { DndMediaProps } from './DndBoard';
 
 const DndImage: React.FC<DndMediaProps> = ({ src, name, style, isSelected, onMouseDown, onClick }) => {
   const [width, setWidth] = useState<number>(style.width as number);
   const [height, setHeight] = useState<number>(style.height as number);
+  const [scale, setScale] = useState<number>(1);
 
   const handleResize = (newWidth: number, newHeight: number) => {
     setWidth(newWidth);
     setHeight(newHeight);
   };
+
+  useEffect(() => {
+    // Calculate the scaling factor based on the initial width and the current width
+    const newScale = width / (style.width as number);
+    setScale(newScale);
+  }, [width, style.width]);
 
   return (
     <Scaler onResize={handleResize}>
@@ -19,7 +26,12 @@ const DndImage: React.FC<DndMediaProps> = ({ src, name, style, isSelected, onMou
         onClick={onClick}
         style={{ ...style, width, height }}
       >
-        <img src={src} alt={name} className="draggable-media" style={{ width: '100%', height: '100%' }} />
+        <img
+          src={src}
+          alt={name}
+          className="draggable-media"
+          style={{ width: '100%', height: '100%', transform: `scale(${scale})` }}
+        />
       </div>
     </Scaler>
   );
