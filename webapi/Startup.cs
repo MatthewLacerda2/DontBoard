e9391cba67536dcc3e09cab47c3e26e85661bcb2
Matheus Lacerda <matheus.l1996@gmail.com>
2024-03-12 18:24:33 -0300

    startup.cs

---
+++ webapi/Startup.cs
@@ -0,0 +1,52 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using MongoDB.Driver;
+
+namespace Server;
+
+public class Startup {
+        
+    public Startup(IConfiguration configuration) {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services) {
+
+        // Configure MongoDB
+        string connectionString = Configuration.GetConnectionString("MongoDbConnection")!;
+        services.AddSingleton<IMongoClient>(new MongoClient(connectionString));
+        services.AddSingleton<IMongoDatabase>(provider => {
+            var client = provider.GetRequiredService<IMongoClient>();
+            return client.GetDatabase("mongo_db");
+        });
+
+        services.AddAuthorization();
+
+        services.AddControllersWithViews();
+
+        // Add Swagger
+        services.AddSwaggerGen(c => {
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "BoardAPI", Version = "v1" });
+        });
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+
+        if (env.IsDevelopment()) {
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Flow API v1"));
+        }
+
+        app.UseHttpsRedirection();
+        app.UseRouting();
+        app.UseAuthorization();
+        app.UseAuthentication();
+
+        app.UseEndpoints(endpoints => {
+            endpoints.MapControllers();
+        });
+    }
+}
