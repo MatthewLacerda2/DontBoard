17f28facae915137d2f5a46c819f6e43dcdde640
Matheus Lacerda <matheus.l1996@gmail.com>
2024-03-17 16:11:21 -0300

    MediaFileDTO

--- webapi/Models/MediaFile.cs
+++ webapi/Models/MediaFile.cs
@@ -2,14 +2,14 @@ namespace Server.Models;
 
 public class MediaFile {
 
-    public mediaFileType fileType;
+    public MediaFileType fileType;
     public readonly Guid Id;
     public string src;
     public string name;
     public string type;
     public int positionX, positionY;
 
-    public MediaFile(mediaFileType _fileType, string _src, string _name, string _type, int x, int y){
+    public MediaFile(MediaFileType _fileType, string _src, string _name, string _type, int x, int y){
         Id = Guid.NewGuid();
         fileType = _fileType;
         src = _src;
